3520fea116f4ceabf83217d77289df47b9b722b2
Nastie <nstalennaya@gmail.com>
2023-10-26 22:01:35 +0300

    name

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from pprint import pprint
+
+url = 'https://script.google.com/macros/s/AKfycbzZ7ofoaQy2pbc7u-IjT71TxscX2GUd5iv7H4EVbXIR6ml32HsBVcG5AXTRPk1rleqGXw/exec'
+
+data = requests.get(url)
+
+data_dict = data.json()
+
+# pprint(data_dict)
+
+money = 0
+
+# names = []
+# for person in data_dict['trip']:
+#    if person['money']:
+#        print(f'{person["name"]} -->> {person["money"]}')
+#        money += person['money']
+#
+#    names.append(person['name'])
+#
+# print(money)
+# print(names)
+
+money = [person['money'] for person in data_dict['trip']]
+money = sum([person['money'] for person in data_dict['trip'] if person['money']])
+pprint(money)
+
+names = {person["name"] for person in data_dict['trip']}
+pprint(names)
+
+names = {person["name"] for person in data_dict['trip']}
+pprint(names)
+
+names = {person["name"]: person["money"] for person in data_dict['trip']}
+pprint(names)
+
+names = {person["name"] for person in data_dict['trip']}
+pprint(names)
